19c7f3b69e5b01fe8fca902216e000341a34642b
Emmett <emmett.fitzharris@mycit.ie>
2023-10-10 12:29:14 +0100

    Project Structure

---
+++ scripts/login.js
@@ -0,0 +1,3 @@
+// Use a cookie to store a string which contains a dictionary of variable.
+// json.stringify() is an option
+
---
+++ scripts/rockPaperScissors.js
@@ -0,0 +1,43 @@
+//main game logic
+
+/*
+
+int playerChoice, computerChoice, winCheck
+playerChoice = getfromform()
+computerChoice = randint(1,3)
+
+if playerChoice == computerChoice
+    do winCheck=0
+
+else if:
+    case: playerChoice==1 && computerChoice==3
+        do winCheck=1
+            case: playerChoice==2 && computerChoice==1
+        do winCheck=1
+            case: playerChoice==3 && computerChoice==2
+        do winCheck=1
+
+else:
+    do winCheck = 2
+
+ */
+
+// use same cookie as login.js to track scores
+
+/*
+
+HTML write playerChoice
+HTML write computerChoice
+HTML write result
+
+activearray = readCookie()
+
+if winCheck == 1:
+    increment playerWinCount
+else if winCheck ==2:
+    increment computerWinCount
+
+write scores
+write cookie
+
+ */
