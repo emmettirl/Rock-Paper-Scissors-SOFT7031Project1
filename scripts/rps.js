0388561db3dead2b738cb93ce32358be0de06266
Emmett <emmett.fitzharris@mycit.ie>
2023-10-20 14:14:07 +0100

    Merged both js files to reduce duplication

--- scripts/rps.js
+++ scripts/rps.js
@@ -1,3 +1,21 @@
+/* Todo:
+
+use same cookie as rps.js to track scores
+
+
+activearray = readCookie()
+
+if winCheck == 1:
+    increment playerWinCount
+else if winCheck ==2:
+    increment computerWinCount
+
+write scores
+write cookie
+
+ */
+
+
 class User {
     constructor(userName, password) {
         this.userName = userName;
@@ -86,6 +104,8 @@ function getAccounts() {
 
 
 function registerNewUser(name, pass){
+    let accountsInstance = getAccounts();
+
     if (name !== "" && pass !== "") {
         let accountExists = 0;
         for (let i = 0; i < accountsInstance.userList.length; i++) {
@@ -123,13 +143,79 @@ function loginExistingUser(name, pass) {
 }
 
 
+function rockPaperScissors(player1Choice, player2Choice){
+    let winner;
+
+    if (player1Choice === player2Choice) {
+        winner = 0;
+    }
+    else if (
+        (player1Choice === "rock" && player2Choice === "scissors")
+        || (player1Choice === "paper" && player2Choice === "rock")
+        || (player1Choice === "scissors" && player2Choice === "paper")
+    ){
+        winner = 1;
+    }
+    else {
+        winner = 2;
+    }
+
+    return winner
+}
+
+function getComputerChoice(){
+    let min = 0;
+    let max = 3;
+    let randInt = Math.floor(Math.random() * (max - min) + min);
+
+    let choice;
+    switch (randInt) {
+        case 0: choice = "rock"; break;
+        case 1: choice = "paper";  break;
+        case 2: choice = "scissors";  break;
+    }
+    return choice
+}
+
+
+function letter0Upper(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function runGame(player1Choice){
+    let cookie = getCookie();
+    if (cookie != ""){
+        playerScore = cookie[1]
+    }
+
+    let player2Choice = getComputerChoice();
+
+    let result = rockPaperScissors(player1Choice, player2Choice);
+    console.log(player2Choice)
+    document.getElementById("playerResult").innerHTML = letter0Upper(player1Choice);
+    document.getElementById("computerResult").innerHTML = letter0Upper(player2Choice);
+    switch (result) {
+        case 0:
+            console.log("tie");
+            document.getElementById("endresult").innerHTML="It's a tie!";
+            break;
+        case 1:
+            console.log("player win");
+            document.getElementById("endresult").innerHTML="You Win!";
+            break;
+        case 2:
+            console.log("computer win");
+            document.getElementById("endresult").innerHTML="You Lose!";
+            break;
+    }
+}
+
 //Main Script Logic
 
 
 //cookie information
-const cName = "RPSAccountInfo"
+const cName = "RpsAccountInfo"
 const expiryDays = 5
 
-accountsInstance = getAccounts(cName)
-console.log(accountsInstance)
+
 
